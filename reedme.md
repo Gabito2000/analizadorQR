c2d67cc2dda402a3c332b67b23a297ace50539d7
Gabriel Kryger <gabriel.kryger@fing.edu.uy>
2022-11-22 09:34:30 -0300

    mando para guardarlo

--- reedme.md
+++ reedme.md
@@ -3,3 +3,9 @@ uvicorn main:app --reload
 
 CREATE THE EXECUTABLE
 python -m PyInstaller --onefile program.py
+
+HOW TO build 
+pip install -r requirements.txt
+
+How to Compile
+pyinstaller --noconfirm --onedir --console --collect-all flasgger "./main.py"
--- server.py
+++ server.py
@@ -1,8 +1,43 @@
-# Import writer class from csv module
-from csv import writer
+from flask import Flask, request 
+from pydantic import BaseModel
 
-# Path: server.py
-def write_to_csv(data):
-    with open('database.csv', 'a') as database:
-        csv_writer = writer(database)
-        csv_writer.writerow(data)
+app = Flask(__name__)
+
+class Person(BaseModel):
+    nombre: str
+    cargo: str
+    entrada: str
+    salida: str
+
+def PersonToCSV(json):
+    return f"{json.nombre},{json.cargo},{json.entrada},{json.salida}"
+
+# adds person to a excel file person
+@app.route("/add_person", methods=["POST"])
+def add_person(person: Person):
+    #open file
+    file = open("database.csv", "a")
+    #write person
+    file.write(PersonToCSV(person) + "\n")
+    #close file
+    file.close()
+    return {"message": "Persona agregada"}
+
+#returns the excel file
+@app.route("/excel", methods=["GET"])
+def excel():
+    file = open("database.csv", "r")
+    return file.read()
+
+#returns index.html
+@app.route("/", methods=["GET"])
+def read_root():
+    #index.html is in the same folder as main.py
+    file = open("index.html", "r")
+    return file.read()
+
+
+#returns qr librery
+@app.route("/getLibrery", methods=["GET"])
+def getLibrery():
+    return open("node_modules\html5-qrcode\html5-qrcode.min.js", "r").read()
